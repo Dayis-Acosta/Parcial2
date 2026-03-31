2fbb3680eaa8b820625eec8c83015d5de33f1a3c
Katherin Tabares <est.katherin.tabares@unimilitar.edu.co>
2026-03-31 15:51:43 -0500

    Prueba de crear el menu de entrada.

--- src/main/java/com/mycompany/tiendadepostres/TiendaDePostres.java
+++ src/main/java/com/mycompany/tiendadepostres/TiendaDePostres.java
@@ -2,10 +2,28 @@
 
 package com.mycompany.tiendadepostres;
 
-
+import com.murcia.utils.*;
 public class TiendaDePostres {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+      final char SALIR = '3';
+        String []opt = {"1. Comanda", "2. Para llevar", "3. Terminar"};
+        Menu mnu = new Menu(opt, 'V', " ", "Tienda de Postes EeveeBuron");
+        char opc;
+
+        do {
+            Consola.clrscr(); // Borra pantalla
+            Consola.gotoxy(0, 0);
+            opc = mnu.select("Su opción [1-3]: ");
+            Input.nextLine("");
+            if (opc == '1'){
+                //Comanda();
+                System.out.println("Comanda");
+            }    
+            if (opc == '2'){
+               //PedidoLevar();
+               System.out.println("pedido llevar");
+            }
+        } while (opc != SALIR);
+    }  
 }
