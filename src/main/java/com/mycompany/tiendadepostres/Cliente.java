645a828cd769a10937b0eb769b211b48ca5061a4
Katherin Tabares <est.katherin.tabares@unimilitar.edu.co>
2026-03-31 15:29:26 -0500

    Se agregaron 3 metodos (setnomCliente, settelCliente, setdirecCliente) en la clase Cliente

--- src/main/java/com/mycompany/tiendadepostres/Cliente.java
+++ src/main/java/com/mycompany/tiendadepostres/Cliente.java
@@ -20,11 +20,20 @@ public class Cliente {
     public String getnomCliente(){
         return nomCliente;
     }
+    public void setnomCliente(String nomCliente){
+        this.nomCliente = nomCliente;
+    }
     public String gettelCliente(){
         return telCliente;
     }
+    public void settelCliente(String telCliente){
+        this.telCliente = telCliente;
+    }
     public String getdirecCliente(){
         return direcCliente;
+    }
+    public void setdirecCliente(String direcCliente){
+         this.direcCliente = direcCliente;
     }
     
     public String toString(){
