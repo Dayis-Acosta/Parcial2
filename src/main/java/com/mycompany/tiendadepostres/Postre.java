1c1765a80cabdb3aee87f5296749c85038dfcab3
Katherin Tabares <est.katherin.tabares@unimilitar.edu.co>
2026-04-01 07:56:45 -0500

    Cambio de nombre de los atributos y creacion de un metodo en la clase Postre. (Realizado por Dayanna)

--- src/main/java/com/mycompany/tiendadepostres/Postre.java
+++ src/main/java/com/mycompany/tiendadepostres/Postre.java
@@ -4,13 +4,16 @@ package com.mycompany.tiendadepostres;
 
 public class Postre {
   //Atributos 
-    private String  nPostre;
-    private int cantidadp;
+    private String  nombrep;
+    private int porciones;
     
-    
+  
     //Metodos
     public Postre (){}
-    
+    public Postre(String nombrep, int porciones) {
+        this.nombrep = nombrep;
+        this.porciones = porciones;
+    }
     public String ListaPostres (String nPostres){
         //se genera la lista de postres
         return nPostres;
