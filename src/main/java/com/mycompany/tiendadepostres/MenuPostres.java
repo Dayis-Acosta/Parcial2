88b4298a7566a69caa78899993e67f42f5ebe128
Katherin Tabares <est.katherin.tabares@unimilitar.edu.co>
2026-04-04 22:18:53 -0500

    Se creo la clase MenuPostre, donde se añadieron los atributos y  unos metodos vacios. (Realizado por Dayanna)

---
+++ src/main/java/com/mycompany/tiendadepostres/MenuPostres.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.tiendadepostres;
+
+public class MenuPostres {
+    private String Pchescake;
+    private String Ptorta;
+    
+    public MenuPostres() {}
+    public MenuPostres() {
+          
+    }
+}
