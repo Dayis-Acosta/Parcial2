90efc0f32244a594674cbdf1e79ab9540d179f45
Katherin Tabares <est.katherin.tabares@unimilitar.edu.co>
2026-04-04 21:14:57 -0500

    Se crearon los metodos de la clase ArmarPedido, pero el metodo AgregarPedido marca error al crear la lista armarPedido.

--- src/main/java/com/mycompany/tiendadepostres/ArmarPedido.java
+++ src/main/java/com/mycompany/tiendadepostres/ArmarPedido.java
@@ -6,5 +6,19 @@ import com.murcia.utils.*;
 public class ArmarPedido {
     private ListaEnlazada armarPedido;
     
+    public ArmarPedido(){}
+    public ArmarPedido(ListaEnlazada armarPedido){
+        this.armarPedido = armarPedido;
+    }
     
+    public void AgregarPedido (Object pedido) {
+        ListaEnlazada armarPedido = new ColaEnlazada;
+        armarPedido.add(pedido);
+    }
+    public String mostrarPedido (){
+        for (int i=0; i<=armarPedido.size(); i++){
+            return armarPedido.get(i) + 
+                    "\n__________________________________";
+        }   
+    }
 }
