c99f69a3cfdb472abaf8991490591fc83b49ac89
Katherin Tabares <est.katherin.tabares@unimilitar.edu.co>
2026-04-04 22:50:54 -0500

    Se crearon los metodos en la clase MenuPostres (Realizado por Dayanna).

--- src/main/java/com/mycompany/tiendadepostres/MenuPostres.java
+++ src/main/java/com/mycompany/tiendadepostres/MenuPostres.java
@@ -10,12 +10,27 @@ public class MenuPostres {
         Pchescake = "Cheesecake";
         Ptorta = "Torta";    
     }
-    /*public String Pchescake (){
-        return maracuya, fresa, mora, coco;
-    }
+    public String Pchescake () {
+		return "maracuya, fresa, mora, coco";
+	}
+
+	public String Ptorta () {
+		return "chocolate, vainilla, tresleches, redvelvet";
+	}
+
+	//metodo de menu mostrar opciones de postres y
+	public String mostrarMenu() {
+		return "1. Cheesecake\n2. Torta";
+	}
+	
+	//Metodo mostrar sabores menu de Cheesecake y Torta
+    public String saboresCheesecake() {
+        return Pchescake + ": Mora, Maracuyá, Coco, Fresa";
+    }    
     
-    public String Ptorta (){
-        return chocolate, vainilla, tresleches, redvelvet;
-    }*/
+    public String saboresTorta(){
+      return Ptorta + ": Chocolate, Vainilla, Tres Leches, Red Velvet";
+
+    }
     
 }
